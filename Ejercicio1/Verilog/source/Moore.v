65cfad088cc4a9b71a57b3b6c3a6347ad0d92313
gcbertachini <gbertachini@itba.edu.ar>
2019-11-15 21:20:28 -0300

    verilog funcionando

--- Ejercicio1/Verilog/source/Moore.v
+++ Ejercicio1/Verilog/source/Moore.v
@@ -3,28 +3,27 @@ module Moore(clk,rst,I,S,Q_1,Q_0);
 
     input clk,rst,I,S;
     output Q_1,Q_0;
-    wire [1:0] Q,Q_sig,E;
-    wire Q_T,Q_T_sig;
+    wire [1:0] Q,D,E;
+    wire Q_T;
     
-    assign Q_1=Q_sig[1];
-    assign Q_0=Q_sig[0];
-    
+
+
 //Intermedian wires
     wire S_neg,I_neg,Q_0S_neg,Q_0I_neg,Q_1S_neg,Q_1I_neg,S_negI_neg,Q_0xorQ_1,Eclock;
-    wire [1:0] Q_sig_neg;
-    wire Q_T_sig_neg;
+    wire [1:0] Q_neg;
+    wire Q_T_neg;
 //Conections
     not(S_neg,S);
     not(I_neg,I);
-    not(Q_sig_neg[0],Q_sig[0]);
-    not(Q_sig_neg[1],Q_sig[1]);
+    not(Q_neg[0],Q_0);
+    not(Q_neg[1],Q_1);
 
 //Conexiones Caso I
     //ANDs
-    and(Q_0S_neg,Q_sig[0],S_neg);
-    and(Q_0I_neg,Q_sig[0],I_neg);
-    and(Q_1S_neg,Q_sig[1],S_neg);
-    and(Q_1I_neg,Q_sig[1],I_neg);
+    and(Q_0S_neg,Q_0,S_neg);
+    and(Q_0I_neg,Q_0,I_neg);
+    and(Q_1S_neg,Q_1,S_neg);
+    and(Q_1I_neg,Q_1,I_neg);
     and(S_negI_neg,I_neg,S_neg);
 
     //ORs
@@ -32,19 +31,19 @@ module Moore(clk,rst,I,S,Q_1,Q_0);
     or(E[0],Q_1S_neg,Q_1I_neg,S_negI_neg);
 
 //Conexiones Caso II
-    xnor(Q_0xorQ_1,Q_sig[0],Q_sig[1]);
+    xnor(Q_0xorQ_1,Q_0,Q_1);
     and(Eclock,I,S_neg,Q_0xorQ_1);
-    flip_flop_T FF_T(Eclock,rst,Q_T,Q_Tsig);
+    flip_flop_T FF_T(Eclock,rst,I,Q_T);
 
-    not(Q_T_sig_neg,Q_T_sig);
+    not(Q_T_neg,Q_T);
 
 
 //Conexión general
-    mux1 mux1_1(Eclock,Q_Tsig,E[1],Q[0]);
-    mux1 mux1_2(Eclock,Q_T_sig_neg,E[0],Q[1]);
+    mux1 mux1_1(Eclock,Q_T,E[1],D[1]);
+    mux1 mux1_2(Eclock,Q_T_neg,E[0],D[0]);
 
-    flip_flop_D FFD_0(clk,rst,Q[0],Q_sig[0]);
-    flip_flop_D FFD_1(clk,rst,Q[1],Q_sig[1]);
+    flip_flop_D FFD_0(clk,rst,D[0],Q_0);
+    flip_flop_D FFD_1(clk,rst,D[1],Q_1);
 
 
 endmodule
@@ -64,15 +63,12 @@ endmodule
 //This is where the flip-flop T starts
 module flip_flop_T(Clock,Reset,T,Q);
     input T, Clock, Reset;
-    output reg Q;
-    always @(posedge Reset, posedge Clock) begin
-        if (Reset == 1)
-            Q <= 0;
-        else
-            if (T)
+    output reg Q=0;
+
+
+    always @(posedge Clock) begin
+        if (T)
                 Q <= ~Q;
-            else
-                Q <= Q;
         end
 endmodule
 
